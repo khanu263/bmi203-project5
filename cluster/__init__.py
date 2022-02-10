97af23a66c8384cf574defd6cf8f4685bbc3ba6b
Umair Khan <umair.khan@ucsf.edu>
2022-02-09 17:54:59 -0800

    Add docstring to __init__.py so flit doesn't complain

--- cluster/__init__.py
+++ cluster/__init__.py
@@ -1,3 +1,8 @@
+"""
+BMI203: Biocomputing algorithms Winter 2022
+Assignment 5: k-means and silhouette scoring
+"""
+
 from .kmeans import KMeans
 from .silhouette import Silhouette
 from .utils import (
